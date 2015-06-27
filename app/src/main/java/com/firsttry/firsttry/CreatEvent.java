47e106353bf70a3ef7de36560821fa22786a00d8
WemyJu <wemyju@gmail.com>
2015-06-27 17:17:27 +0800

    first commit

---
+++ app/src/main/java/com/firsttry/firsttry/CreatEvent.java
@@ -0,0 +1,20 @@
+package com.firsttry.firsttry;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class CreatEvent extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_creat_event, container, false);
+
+        return rootView;
+    }
+}
---
+++ app/src/main/java/com/firsttry/firsttry/PositionInfo.java
@@ -0,0 +1,20 @@
+package com.firsttry.firsttry;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class PositionInfo extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_position_info, container, false);
+
+        return rootView;
+    }
+}
